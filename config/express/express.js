907b5f9e07cd6c31934c6d4c44ea937139bca80f
ananay-nag <ananay-nag@github.com>
2020-11-10 17:26:31 +0530

    all done

--- config/express/express.js
+++ config/express/express.js
@@ -1,5 +1,7 @@
 const BodyParser = require("body-parser");
+const cors = require("cors");
 module.exports = function (app) {
   app.use(BodyParser.urlencoded({ extended: false }));
+  app.use(cors());
   app.use(BodyParser.json());
 };
