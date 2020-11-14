88eb2ff4a520ad38743e294fa79c35617274d213
ANANYA NAG <36204888+ananay-nag@users.noreply.github.com>
2020-11-14 16:49:04 +0530

    Update db.js

--- config/db/db.js
+++ config/db/db.js
@@ -1,5 +1,5 @@
 const mongo = require("mongoose");
-const url = "mongodb+srv://testUser:testUser123@cluster0.aj3jl.mongodb.net/eventDB?retryWrites=true&w=majority";
+const url = "mongodb+srv://testUser:<pwd>@cluster0.aj3jl.mongodb.net/eventDB?retryWrites=true&w=majority";
 // export db connection function;
 module.exports = { dbConnect: dbConnect };
 function dbConnect() {
